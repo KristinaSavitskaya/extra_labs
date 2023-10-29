ab4a192aadf64f48d77c53d8de1fd816dd915d08
KristinaSavitskaya <148622003+KristinaSavitskaya@users.noreply.github.com>
2023-10-29 18:41:13 +0600

    Create lab2_task1.py

---
+++ lab2/lab2_task1.py
@@ -0,0 +1,5 @@
+triangle_side1 = int(input("Введите первый катет: "))
+triangle_side2 = int(input("Введите второй катет: "))
+
+triangle_side3 = (triangle_side1**2 + triangle_side2**2)**0.5
+print(f"Гипотенуза равняется {triangle_side3}")
